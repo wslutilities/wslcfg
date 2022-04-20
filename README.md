cf21174582eab9d1f9d8dae8fb5ffb9ad09253ab
Jinming Wu, Patrick <me@patrickwu.space>
2022-04-20 12:17:22 +0800

    add test ini reader

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wslconf
 
-This is a component of wslu. This is the first component written in c for wslu in the future wslu 3.0 release. This repo will be stop developing once it is merged into the wslu develop branch.
+This is a component of wslu. 
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::env;
-use serde_json;
 use std::fs::File;
 use std::io::BufReader;
+
+extern crate ini;
+use ini::Ini;
+
+use serde_json;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let file = File::open("src/default.json").expect("failed to read file");
-    let reader = BufReader::new(file);
+    let conf = Ini::load_from_file("/etc/wsl.conf").unwrap();
+
+    let default_file =
+        File::open("src/default.json")
+            .expect("failed to read file");
+    let reader = BufReader::new(default_file);
     let default_set: serde_json::Value =
         serde_json::from_reader(reader)
             .expect("JSON was not well-formatted");
-    println!("{:?}", default_set);
 
+    println!("{:?}", default_set);
 }
